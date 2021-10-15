bd6bb9aaddf721d8b32bb7baf3e9beb4b0333038
koen-schouten <schouten_koen@hotmail.com>
2021-10-15 12:51:08 +0200

    Added a repeatable bash script that exucutes the python script and pushes the changes to github.

---
+++ repeatscript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#run the python script
+python /app/COVID19-Dataset-Downloader/src/covid_data_downloader.py
+
+#change to the dataset dir
+cd /app/COVID19-Dataset-Netherlands
+
+#commit the changes made by the python script
+git add .
+git commit -m "scripted update"
+
+#push the update to github
+git push -u origin main
